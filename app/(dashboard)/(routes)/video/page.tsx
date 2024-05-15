cc2d1915ec5c06719944b7cce49af0072c75deb6
Neander de Souza <neander.faria@hotmail.com>
2024-05-15 10:53:35 -0300

    Video Generation AI (Replicate AI)

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import Empty from "@/components/empty";
+import Heading from "@/components/heading";
+import Loader from "@/components/loader";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { formSchema } from "./constants";
+
+export default function VideoPage() {
+    const router = useRouter();
+    const [video, setVideo] = useState<string>();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: "",
+        },
+    });
+
+    const { handleSubmit, reset, formState: { isSubmitting } } = form;
+
+    async function handleOnSubmit(values: z.infer<typeof formSchema>) {
+        try {
+            setVideo(undefined);
+
+            const response = await axios.post("/api/video", values);
+
+            setVideo(response.data[0]);
+
+            reset();
+        } catch (error: any) {
+            // TODO: Open Pro Model
+            console.log(error);
+        } finally {
+            router.refresh();
+        }
+    }
+
+    return (
+        <div>
+            <Heading
+                title="Video Generation"
+                description="Turn your prompt into video."
+                icon={VideoIcon}
+                iconColor="text-orange-700"
+                bgColor="bg-orange-700/10"
+            />
+
+            <div className="px-4 lg:px-8">
+                <div>
+                    <Form {...form}>
+                        <form
+                            onSubmit={handleSubmit(handleOnSubmit)}
+                            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+                        >
+                            <FormField
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-10">
+                                        <FormControl className="m-0 p-0">
+                                            <Input
+                                                className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                                                disabled={isSubmitting}
+                                                placeholder="Clown fish swimming around a coral reef"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+
+                            <Button
+                                className="col-span-12 lg:col-span-2 w-full"
+                                disabled={isSubmitting}
+                            >
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </div>
+
+                <div className="space-y-4 mt-4">
+                    {isSubmitting && (
+                        <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+                            <Loader />
+                        </div>
+                    )}
+
+                    {!video && !isSubmitting && (
+                        <Empty
+                            label="No video generated."
+                        />
+                    )}
+
+                    {video && (
+                        <video controls className="w-full aspect-video mt-8 rounded-lg border bg-black">
+                            <source src={video} />
+                        </video>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
